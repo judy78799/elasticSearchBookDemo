128500f82cc3aaf78ce3193b428cc94291871ab1
judy78799 <judice0465@gmail.com>
2025-12-30 15:42:57 +0900

    chore: useState 실습

--- react-ui/src/App.jsx
+++ react-ui/src/App.jsx
@@ -1,14 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import {useEffect, useState} from 'react';
 
+const heavyWork = () => {
+    console.log("엄청 무거운 작업!!!");
+    return ["홍길동", "김민수"];
+}
 function App() {
-  const [count, setCount] = useState(0)
+    const [names, setNames] = useState(() => {
+        return heavyWork();
+    });
+    const [input, setInput] = useState('');
+    const handleInputChange = (e) => {
+        setInput(e.target.value);
+    }
+
+    const handleUpload = () => {
+        setNames((prevState) => {   //함수형 업데이트, prevState는 React가 자동으로 전달하는 이전 상태 값
+            console.log('이전 state', prevState);
+            return [input, ...prevState];
+        });
+    };
+    console.log(input);
+
 
   return (
     <div>
+        <input type="text" value={input} onChange={handleInputChange}/>
+        <button onClick={handleUpload}>Upload</button>
+        {names.map((name, idx) => {
+            return <p key={idx}>{name + idx}</p>
+        })}
+
     </div>
+
   )
 }
 
