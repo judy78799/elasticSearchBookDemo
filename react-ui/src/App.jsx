5e6511ab1bcdce0a4caf7d3efefc8634bf0f40f6
judy78799 <judice0465@gmail.com>
2025-12-31 10:51:13 +0900

    chore: useRef 데모

--- react-ui/src/App.jsx
+++ react-ui/src/App.jsx
@@ -1,12 +1,22 @@
 import './App.css'
-import React,{useEffect, useState} from 'react';
-import Timer from './component/Timer.jsx';
+import React,{useEffect, useState, useRef} from 'react'
+
+//useState는 화면 전체 렌더링을 진행. Ref 올린 후 State를 누르게 되면 값이 화면에 렌더링 되어서 나타남.
 
 function App() {
-    const [showTimer, setShowTimer] = useState(false);
+    const [count, setCount] = useState(0);
+  const countRef = useRef(0);
+  console.log(countRef);    //countRef.current 현재 값 확인
+  const increaseCountState = () => {
+    setCount(count + 1);
+  };
+  const increaseCountRef = () => {
+    countRef.current = countRef.current + 1;
+    console.log('Ref: ', countRef.curent);
+  };
   return (
-    <div>{showTimer && <Timer/>}
-        <button onClick={() => setShowTimer(!showTimer)}>Toggle Timer</button>
+    <div>
+        <p>State: {count}</p> <p>Ref: {countRef.current}</p> <button onClick={increaseCountState}>State 올려</button> <button onClick={increaseCountRef}>Ref 올려</button>
     </div>
   );
 }
