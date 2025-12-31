a88e74c32a6bb36a2a3d71871f60223e22e46fea
judy78799 <judice0465@gmail.com>
2025-12-31 14:29:03 +0900

    chore: useRef DOM요소 제어 데모

--- react-ui/src/App.jsx
+++ react-ui/src/App.jsx
@@ -1,29 +1,25 @@
 import './App.css'
-import React,{useEffect, useState, useRef} from 'react'
+import React, {useEffect, useState, useRef, use} from 'react'
 
 //useRef: 렌더링 이후에도 값이 그대로 남아있음. useState 이후 값이 렌더링되어 화면에 나타남.
 //useState: 렌더링 이후 값 초기화.
 
 function App() {
-    const [count, setCount] = useState(1);
-    const renderCount = useRef(1);
+    const inputRef = useRef();
 
-    //[error case]: 무한 렌더링 발생 useEffect -> setRenderCount 업데이트 -> useEffect 무한 반복
-    // useEffect(() => {
-    //     console.log('렌더링!');
-    //     setRenderCount(renderCount + 1);
-    // });
+    useEffect(()=>{
+        console.log(inputRef);
+    },[]); //한번만 실행
 
-    useEffect(() => {
-    // 화면 최초 렌더링
-        renderCount.current = renderCount.current + 1;
-        console.log('렌더링 수: ', renderCount.current);
-    })
+    const login = () => {
+        alert(`환영합니다 ${inputRef.current.value}!`);
+        inputRef.current.focus();
+    }
 
     return (
         <div>
-          <p>Count: {count}</p>
-          <button onClick={() => setCount(count + 1)}>올려</button>
+            <input ref={inputRef} type="text" placeholder="username"/>
+            <button>로그인</button>
         </div>
     );
 };
