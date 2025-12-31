d85c6c703df2e160a5434e42f48d90827d8445db
judy78799 <judice0465@gmail.com>
2025-12-31 11:23:04 +0900

    chore: useRef와 일반 변수의 차이 데모(렌더링 후 값의 변화)

--- react-ui/src/App.jsx
+++ react-ui/src/App.jsx
@@ -1,22 +1,40 @@
 import './App.css'
 import React,{useEffect, useState, useRef} from 'react'
 
-//useState는 화면 전체 렌더링을 진행. Ref 올린 후 State를 누르게 되면 값이 화면에 렌더링 되어서 나타남.
+//useRef: 렌더링 이후에도 값이 그대로 남아있음. useState 이후 값이 렌더링되어 화면에 나타남.
+//useState: 렌더링 이후 값 초기화.
 
 function App() {
-    const [count, setCount] = useState(0);
-  const countRef = useRef(0);
+    const [renderer, setRenderer] = useState(0);
+     const countRef = useRef(0);
+     let countVar = 0;
   console.log(countRef);    //countRef.current 현재 값 확인
-  const increaseCountState = () => {
-    setCount(count + 1);
-  };
-  const increaseCountRef = () => {
-    countRef.current = countRef.current + 1;
-    console.log('Ref: ', countRef.curent);
-  };
+   const doRendering = () => {
+    setRenderer(renderer + 1);
+   }
+
+    const increaseRef = () => {
+        countRef.current = countRef.current + 1;
+        console.log('ref: ', countRef.current);
+    }
+
+    const increaseVar = () => {
+        countVar = countVar + 1;
+        console.log('var: ', countVar);
+    };
+
+    const printResults = () => {
+        console.log(`ref: ${countRef.current}, var: ${countVar}`);
+    }
+
   return (
     <div>
-        <p>State: {count}</p> <p>Ref: {countRef.current}</p> <button onClick={increaseCountState}>State 올려</button> <button onClick={increaseCountRef}>Ref 올려</button>
+        <p>Ref: {countRef.current}</p>
+        <p>Var: {countVar}</p>
+        <button onClick={doRendering}>렌더!</button>
+        <button onClick={increaseRef}>Ref 올려</button>
+        <button onClick={increaseVar}>Var 올려</button>
+        <button onClick={doRendering}>Ref Var 값 출력</button>
     </div>
   );
 }
